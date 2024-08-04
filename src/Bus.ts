6437490976e0ecd176131097998747c2fa5b52be
Volkmar Nissen <volkmar.nissen@gmail.com>
2024-08-04 08:14:30 +0200

    Format all files

--- src/Bus.ts
+++ src/Bus.ts
@@ -1,9 +1,8 @@
-import { IBus, IRTUConnection, ITCPConnection } from "./types";
+import { IBus, IRTUConnection, ITCPConnection } from './types'
 
 export function getBusName(bus: IBus): string {
-    let serialport = (bus.connectionData as IRTUConnection).serialport
-    if (serialport)
-        return serialport
-    let tcpname = (bus.connectionData as ITCPConnection).host + ":" + (bus.connectionData as ITCPConnection).port
-    return tcpname
+  let serialport = (bus.connectionData as IRTUConnection).serialport
+  if (serialport) return serialport
+  let tcpname = (bus.connectionData as ITCPConnection).host + ':' + (bus.connectionData as ITCPConnection).port
+  return tcpname
 }
--- src/types.ts
+++ src/types.ts
@@ -1,118 +1,123 @@
-
-import { IClientOptions } from 'mqtt';
-import { ISpecificationTexts, IbaseSpecification, IdentifiedStates, IimageAndDocumentUrl, ImodbusEntityIdentification, SpecificationStatus } from '@modbus2mqtt/specification.shared';
+import { IClientOptions } from 'mqtt'
+import {
+  ISpecificationTexts,
+  IbaseSpecification,
+  IdentifiedStates,
+  IimageAndDocumentUrl,
+  ImodbusEntityIdentification,
+  SpecificationStatus,
+} from '@modbus2mqtt/specification.shared'
 
 export enum HttpErrorsEnum {
-    OK = 200,
-    OkCreated = 201,
-    OkAccepted = 202,
-    OkNonAuthoritativeInformation = 203,
-    OkNoContent = 204,
-    ErrBadRequest = 400,
-    ErrUnauthorized = 401,
-    ErrForbidden = 403,
-    ErrNotFound = 404,
-    ErrNotAcceptable = 406,
-    ErrRequestTimeout = 408,
-    ErrInvalidParameter = 422,
-    SrvErrInternalServerError = 500
+  OK = 200,
+  OkCreated = 201,
+  OkAccepted = 202,
+  OkNonAuthoritativeInformation = 203,
+  OkNoContent = 204,
+  ErrBadRequest = 400,
+  ErrUnauthorized = 401,
+  ErrForbidden = 403,
+  ErrNotFound = 404,
+  ErrNotAcceptable = 406,
+  ErrRequestTimeout = 408,
+  ErrInvalidParameter = 422,
+  SrvErrInternalServerError = 500,
 }
 export enum RoutingNames {
-    login = 'login',
-    register = 'register',
-    configure = 'configure',
-    busses = 'busses',
-    specifications = 'specifications',
-    slaves = 'slaves',
-    specification = 'specification'
+  login = 'login',
+  register = 'register',
+  configure = 'configure',
+  busses = 'busses',
+  specifications = 'specifications',
+  slaves = 'slaves',
+  specification = 'specification',
 }
 export interface ImqttClient extends IClientOptions {
-    mqttserverurl?: string,
-    ssl?: boolean
+  mqttserverurl?: string
+  ssl?: boolean
 }
 
 export interface IRTUConnection {
-    serialport: string,
-    baudrate: number,
-    timeout: number
+  serialport: string
+  baudrate: number
+  timeout: number
 }
 export interface ITCPConnection {
-    host: string,
-    port: number,
-    timeout: number
+  host: string
+  port: number
+  timeout: number
 }
 
-export type IModbusConnection = (IRTUConnection | ITCPConnection);
+export type IModbusConnection = IRTUConnection | ITCPConnection
 
 export interface Iconfiguration {
-    hassiotoken?: string;
-    password?: string;
-    username?: string;
-    githubPersonalToken?: string;
-    version: string,
-    fakeModbus: boolean;
-    mqttbasetopic: string;
-    mqttdiscoveryprefix: string;
-    mqttdiscoverylanguage: string;
-    mqttusehassiotoken?: boolean;
-    mqttconnect: ImqttClient;
-    mqttcaFile?: string;
-    mqttkeyFile?: string;
-    mqttcertFile?: string;
-    filelocation: string;
-    httpport: number;
-    rootUrl?: string;
+  hassiotoken?: string
+  password?: string
+  username?: string
+  githubPersonalToken?: string
+  version: string
+  fakeModbus: boolean
+  mqttbasetopic: string
+  mqttdiscoveryprefix: string
+  mqttdiscoverylanguage: string
+  mqttusehassiotoken?: boolean
+  mqttconnect: ImqttClient
+  mqttcaFile?: string
+  mqttkeyFile?: string
+  mqttcertFile?: string
+  filelocation: string
+  httpport: number
+  rootUrl?: string
 }
 export enum AuthenticationErrors {
-    EnvironmentVariableSecretNotSet = 1,
-    HashError = 2,
-    InvalidUserPasswordCombination = 3,
-    InvalidParameters = 4,
-    SignError = 5
+  EnvironmentVariableSecretNotSet = 1,
+  HashError = 2,
+  InvalidUserPasswordCombination = 3,
+  InvalidParameters = 4,
+  SignError = 5,
 }
 
 export interface IUserAuthenticationStatus {
-    registered: boolean,
-    hassiotoken: boolean,
-    hasAuthToken: boolean,
-    authTokenExpired: boolean,
-    mqttConfigured: boolean,
-    preSelectedBusId?: number
+  registered: boolean
+  hassiotoken: boolean
+  hasAuthToken: boolean
+  authTokenExpired: boolean
+  mqttConfigured: boolean
+  preSelectedBusId?: number
 }
 export interface IBus {
-    busId: number,
-    connectionData: IModbusConnection,
-    slaves: Islave[]
+  busId: number
+  connectionData: IModbusConnection
+  slaves: Islave[]
 }
 export function getConnectionName(connection: IModbusConnection): string {
-    if ((connection as IRTUConnection).baudrate) {
-        let c = (connection as IRTUConnection);
-        return "RTU: " + c.serialport + "(" + c.baudrate + ") t: " + c.timeout;
-    }
-    else {
-        let c = (connection as ITCPConnection);
-        return "TCP: " + c.host + ":" + c.port + " t: " + (c.timeout ? c.timeout : 100);
-    }
+  if ((connection as IRTUConnection).baudrate) {
+    let c = connection as IRTUConnection
+    return 'RTU: ' + c.serialport + '(' + c.baudrate + ') t: ' + c.timeout
+  } else {
+    let c = connection as ITCPConnection
+    return 'TCP: ' + c.host + ':' + c.port + ' t: ' + (c.timeout ? c.timeout : 100)
+  }
 }
 
 export interface Islave {
-    slaveid: number;
-    specificationid?: string;
-    name?: string;
-    polInterval?: number;
-    specification?: IbaseSpecification;
-    durationOfLongestModbusCall?: number;
-    modbusTimout?: number;
-    evalTimeout?: boolean;
-    detectSpec?: boolean; // Will be set when creating a slave. If true, modbus2mqtt will set a specification matching to the modbusdata if there is one
+  slaveid: number
+  specificationid?: string
+  name?: string
+  polInterval?: number
+  specification?: IbaseSpecification
+  durationOfLongestModbusCall?: number
+  modbusTimout?: number
+  evalTimeout?: boolean
+  detectSpec?: boolean // Will be set when creating a slave. If true, modbus2mqtt will set a specification matching to the modbusdata if there is one
 }
 
 export interface IidentificationSpecification {
-    filename: string;
-    status: SpecificationStatus;
-    entities: ImodbusEntityIdentification[];
-    files: IimageAndDocumentUrl[];
-    i18n: ISpecificationTexts[];
-    identified: IdentifiedStates;
-    configuredSlave?: Islave;
+  filename: string
+  status: SpecificationStatus
+  entities: ImodbusEntityIdentification[]
+  files: IimageAndDocumentUrl[]
+  i18n: ISpecificationTexts[]
+  identified: IdentifiedStates
+  configuredSlave?: Islave
 }
